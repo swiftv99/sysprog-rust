21e0000fe440945612577e59d27454f383b2a8f1
Nils Asmussen <nils@os.inf.tu-dresden.de>
2024-09-24 12:12:01 +0200

    Added solution for books exercise.

--- books/src/main.rs
+++ books/src/main.rs
@@ -1,9 +1,28 @@
 use std::io;
 use std::io::Write;
 
-struct Book {}
+struct Book {
+    title: String,
+    year: u32,
+}
 
-enum Command {}
+impl Book {
+    fn new(title: String) -> Self {
+        Self {
+            title,
+            year: 0,
+        }
+    }
+}
+
+enum Command {
+    Add(String),
+    Year(String, u32),
+    Rem(String),
+    Show,
+    Help,
+    Quit,
+}
 
 fn get_cmd() -> Command {
     // print prompt (ignore errors)
@@ -12,8 +31,18 @@ fn get_cmd() -> Command {
 
     let mut command = String::new();
     io::stdin().read_line(&mut command).unwrap();
-    // TODO parse line and create Command
-    unimplemented!();
+ 
+    let words: Vec<&str> = command.split_whitespace().collect();
+
+    match words[0] {
+        "add" => Command::Add(words[1].to_string()),
+        "year" => Command::Year(words[1].to_string(), words[2].parse().unwrap()),
+        "rem" => Command::Rem(words[1].to_string()),
+        "show" => Command::Show,
+        "help" => Command::Help,
+        "quit" => Command::Quit,
+        _ => panic!("Unexpected command {}", words[0]), 
+    }
 }
 
 fn cmd_help() {
@@ -34,6 +63,22 @@ fn main() {
     loop {
         let command = get_cmd();
 
-        // TODO handle command
+        match command {
+            Command::Add(title) => books.push(Book::new(title)),
+            Command::Year(title, year) => {
+                let book = books.iter_mut().find(|b| b.title == title).unwrap();
+                book.year = year;
+            },
+            Command::Rem(title) => {
+                books.retain(|b| b.title != title);
+            },
+            Command::Show => {
+                for b in &books {
+                    println!("{} (published {})", b.title, b.year);
+                }
+            },
+            Command::Help => cmd_help(),
+            Command::Quit => break,
+        }
     }
 }
